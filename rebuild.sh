4f4e466a22a0ff7e55c6321d157a3f8ebf3a459d
MQ <mq@4mq.xyz>
2024-01-21 13:14:04 +0100

    unify rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
-TARGET_HOSTNAME="nixos-desktop"
+
+TARGET_HOSTNAME="$(uname -n)"
+TARGET_REBUILD=$1
+
+if [ -z "$1" ]; then
+    echo "Specify hostname as first arg"
+    exit 1
+fi
+
 if [ "$HOSTNAME" != "$TARGET_HOSTNAME" ]; then
     read -p "You are running from $HOSTNAME while this script targets $TARGET_HOSTNAME, are you sure? (y): ? " -n 1 -r
     if [ "$REPLY" != "y" ]; then
@@ -7,4 +15,4 @@ if [ "$HOSTNAME" != "$TARGET_HOSTNAME" ]; then
     fi
     echo
 fi
-nixos-rebuild switch --flake path:.#nixos-desktop
+nixos-rebuild switch --flake "path:.#$TARGET_REBUILD"
