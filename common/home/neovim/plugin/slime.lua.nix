22bb20c6f6c99cc025e1c8daa376c694725883b6
MQ <mq@4mq.xyz>
2024-02-01 22:08:15 +0100

    fix slime indent ipy

--- common/home/neovim/plugin/slime.lua.nix
+++ common/home/neovim/plugin/slime.lua.nix
@@ -2,4 +2,5 @@
 vim.keymap.set("n", "<leader>z", "<Plug>SlimeLineSend")
 vim.keymap.set("v", "<leader>z", "<Plug>SlimeRegionSend")
 vim.g.slime_target = "zellij"
+vim.g.slime_bracketed_paste = 1
 ''
