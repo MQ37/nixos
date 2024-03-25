8a02e62e0ed17fd5d154833f23b21fa72373f8f8
MQ <me@kopecky.io>
2024-03-25 17:23:41 +0100

    vim more git keymaps

--- common/home/neovim/plugin/fugitive.lua.nix
+++ common/home/neovim/plugin/fugitive.lua.nix
@@ -1,3 +1,8 @@
 ''
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gw", vim.cmd.Gwrite)
+vim.keymap.set("n", "<leader>gd", vim.cmd.GDelete)
+vim.keymap.set("n", "<leader>gb", function()
+    vim.cmd('Git blame')
+end)
 ''
