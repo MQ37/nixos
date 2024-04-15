fb071e489b41c012695a82a54d154e5df5d9e9e1
MQ <me@kopecky.io>
2024-04-15 09:57:19 +0200

    remap vim fugitive

--- common/home/neovim/plugin/fugitive.lua.nix
+++ common/home/neovim/plugin/fugitive.lua.nix
@@ -1,8 +1,8 @@
 ''
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gw", vim.cmd.Gwrite)
-vim.keymap.set("n", "<leader>gd", vim.cmd.GDelete)
-vim.keymap.set("n", "<leader>gb", function()
+vim.keymap.set("n", "<leader>gfs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gfw", vim.cmd.Gwrite)
+vim.keymap.set("n", "<leader>gfd", vim.cmd.GDelete)
+vim.keymap.set("n", "<leader>gfb", function()
     vim.cmd('Git blame')
 end)
 ''
