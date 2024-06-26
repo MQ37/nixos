42352c2f1fac61fcda805b3944b4d13d1e32f32c
MQ <me@kopecky.io>
2024-06-26 14:08:38 +0200

    add steam

--- common/configuration.nix
+++ common/configuration.nix
@@ -119,6 +119,15 @@
     which
   ];
 
+  # steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
